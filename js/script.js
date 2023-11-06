8d89a54c0636805f5b4a686608d7d327625e2fa8
Tanishq Tiwari <tan2208.gkp@gmail.com>
2023-11-07 00:31:46 +0530

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,87 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+// Close navbar when link is clicked
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
+
+// Event Listeners: Handling toggle event
+const toggleSwitch = document.querySelector(
+  '.theme-switch input[type="checkbox"]'
+);
+
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+  }
+}
+
+toggleSwitch.addEventListener("change", switchTheme, false);
+
+//  Store color theme for future visits
+
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark"); //add this
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light"); //add this
+  }
+}
+
+// Save user preference on load
+
+const currentTheme = localStorage.getItem("theme")
+  ? localStorage.getItem("theme")
+  : null;
+
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme);
+
+  if (currentTheme === "dark") {
+    toggleSwitch.checked = true;
+  }
+}
+
+//Adding date
+
+let myDate = document.querySelector("#datee");
+
+const yes = new Date().getFullYear();
+myDate.innerHTML = yes;
+
+// New JS added
+// const image = document.querySelector('.click1');
+// const card = document.getElementById('project-detail-id');
+
+// image.addEventListener('click', (e) => {
+//   card.style.display = 'block';
+// });
+
+// // Hide the card when clicking outside of it
+// document.body.addEventListener('click', () => {
+//   card.style.display = 'none';
+// });
+
+const image = document.getElementById('click1');
+const details = document.getElementById('project-detail-id');
+
+image.addEventListener('click', (e) => {
+  details.style.display = 'block';
+})
